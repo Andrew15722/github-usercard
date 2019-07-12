802677f5a204ff8c61e06cfa1eb2b6117472a716
andrew-mendez <andrew15722@yahoo.com>
2019-07-11 17:02:30 -0700

    met mvp finished setting the content so it would should on the webpage. added the content inside the axios call

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -24,7 +24,7 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [ 'tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell' ];
+const followersArray = [ 'tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell', 'Andrew15722' ];
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -53,16 +53,6 @@ followersArray.forEach((follower) => {
 });
 
 function createCard(username) {
-	axios
-		.get(`https://api.github.com/users/${username}`)
-		.then((data) => {
-			console.log('it works', data);
-			return data;
-		})
-		.catch((error) => {
-			console.log('API currently down', error);
-		});
-
 	// creating the elements
 	const card = document.createElement('div');
 	const cardImg = document.createElement('img');
@@ -71,7 +61,7 @@ function createCard(username) {
 	const cardUserName = document.createElement('p');
 	const cardLocation = document.createElement('p');
 	const cardProfile = document.createElement('p');
-	const cardProfileAddress = document.createElement('p');
+	const cardProfileAddress = document.createElement('a');
 	const cardFollowers = document.createElement('p');
 	const cardFollowing = document.createElement('p');
 	const cardBio = document.createElement('p');
@@ -94,16 +84,24 @@ function createCard(username) {
 	cardName.classList.add('name');
 	cardUserName.classList.add('username');
 
-	// set content
-	cardImg.src = data.data.avatar_url;
-	cardName.textContent = data.data.name;
-	cardUserName.textContent = data.data.login;
-	cardLocation.textContent = data.data.location;
-	cardProfileAddress.href = data.data.url;
-	cardProfileAddress.textContent = data.data.url;
-	cardFollowers.textContent = data.data.followers;
-	cardFollowing.textContent = data.data.following;
-	cardBio.textContent = data.data.bio;
+	// set content.
+	axios
+		.get(`https://api.github.com/users/${username}`)
+		.then((data) => {
+			console.log('it works', data);
+			cardImg.src = data.data.avatar_url;
+			cardName.textContent = data.data.name;
+			cardUserName.textContent = data.data.login;
+			cardLocation.textContent = `Location: ${data.data.location}`;
+			cardProfileAddress.href = `https://github.com/${username}`;
+			cardProfileAddress.textContent = `https://github.com/${username}`;
+			cardFollowers.textContent = `Followers: ${data.data.followers}`;
+			cardFollowing.textContent = ` Following: ${data.data.following}`;
+			cardBio.textContent = `Bio: ${data.data.bio}`;
+		})
+		.catch((error) => {
+			console.log('API currently down', error);
+		});
 
 	return card;
 }
